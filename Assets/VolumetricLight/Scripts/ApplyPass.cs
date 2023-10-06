f26a24fcea367198d6d89498b46be16102578728
avi9111@163.com <avi9111@163.com>
2023-10-06 17:08:27 +0800

    第一次add

---
+++ Assets/VolumetricLight/Scripts/ApplyPass.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class ApplyPass : ScriptableRenderPass
+{
+    private readonly string k_BlitAddTag = "VolumetricLight Blit Add RenderPass";
+
+    public VolumtericResolution m_VolumtericResolution;
+    public VolumetricLightFeature m_Feature;
+
+    private Material m_ApplyMaterial;
+    private CommandBuffer m_ApplyCommand;
+    private RenderTargetHandle m_BlurTempTex;
+    private RenderTargetHandle m_FullRayMarchTex;
+    RenderTargetHandle m_TemporaryColorTexture;
+    public ApplyPass()
+    {
+        renderPassEvent = RenderPassEvent.BeforeRenderingPostProcessing;
+        m_ApplyMaterial = CoreUtils.CreateEngineMaterial("Hidden/Apply");
+        int downSampleCout = m_VolumtericResolution == VolumtericResolution.Full ? 1 : m_VolumtericResolution == VolumtericResolution.Half ? 2 : 4;
+        m_ApplyMaterial.SetInt("_DownSampleCount", downSampleCout);
+        m_BlurTempTex.Init("BlurTempTex");
+        m_FullRayMarchTex.Init("FullRayMarchTex");
+        m_TemporaryColorTexture.Init("m_TemporaryColorTexture");
+        m_TempColorTexHandle = RTHandles.Alloc("m_TempColorTexHandle");
+    }
+    ScriptableRenderer m_Renderer;
+    public void Setup(ScriptableRenderer renderer)
+    {
+        m_Renderer = renderer;
+    }
+    public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+    {
+        base.Configure(cmd, cameraTextureDescriptor);
+        cmd.GetTemporaryRT(m_FullRayMarchTex.id, cameraTextureDescriptor, FilterMode.Bilinear);
+
+        RenderTextureDescriptor descriptor = cameraTextureDescriptor;
+        if (m_VolumtericResolution == VolumtericResolution.Half)
+        {
+            descriptor.width /= 2;
+            descriptor.height /= 2;
+        }
+        else if (m_VolumtericResolution == VolumtericResolution.Quarter) {
+            descriptor.width /= 4;
+            descriptor.height /= 4;
+        }
+        cmd.GetTemporaryRT(m_BlurTempTex.id, descriptor, FilterMode.Bilinear);
+        cmd.GetTemporaryRT(m_TemporaryColorTexture.id, descriptor, FilterMode.Bilinear);
+
+    }
+    public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+    {
+        if (m_ApplyMaterial == null) return;
+        m_ApplyCommand = CommandBufferPool.Get(k_BlitAddTag);
+        context.ExecuteCommandBuffer(m_ApplyCommand);
+        m_ApplyCommand.Clear();
+        Render(m_ApplyCommand, ref renderingData);
+        context.ExecuteCommandBuffer(m_ApplyCommand);
+        CommandBufferPool.Release(m_ApplyCommand);
+    }
+    RTHandle m_TempColorTexHandle;
+    void Render(CommandBuffer cmd, ref RenderingData renderingData)
+    {
+        if (renderingData.cameraData.isSceneViewCamera) return;
+
+
+        //cmd.SetGlobalTexture("_SourceTex", m_Renderer.cameraColorTargetHandle);
+        cmd.SetGlobalTexture("_SourceTex", m_Renderer.cameraColorTarget);
+        Blit(cmd, m_TempColorTexHandle, m_TemporaryColorTexture.Identifier(), m_ApplyMaterial);
+        //Blit(cmd, m_TemporaryColorTexture.Identifier(), m_Renderer.cameraColorTargetHandle);
+        Blit(cmd, m_TemporaryColorTexture.Identifier(), m_Renderer.cameraColorTarget);
+
+    }
+    public override void FrameCleanup(CommandBuffer cmd)
+    {
+        cmd.ReleaseTemporaryRT(m_BlurTempTex.id);
+        cmd.ReleaseTemporaryRT(m_FullRayMarchTex.id);
+        cmd.ReleaseTemporaryRT(m_TemporaryColorTexture.id);
+        m_TempColorTexHandle.Release();
+    }
+}
---
+++ Assets/VolumetricLight/Scripts/VolumetricLightFeature.cs
@@ -0,0 +1,65 @@
+using UnityEngine.Rendering.Universal;
+using UnityEngine.Rendering;
+using UnityEngine;
+
+public enum VolumtericResolution {
+    Full,
+    Half,
+    Quarter
+}
+
+public class VolumetricLightFeature : ScriptableRendererFeature
+{
+    [System.Serializable, ReloadGroup]
+    public class Settings
+    {
+        public VolumtericResolution resolution;
+        public Color TintColor = Color.white;
+        [Range(0.0f, 1.0f)]
+        public float FogHeightCoef = 0.0f;
+        [Range(0.0f, 2.0f)]
+        public float ShadowProjCoef = 0.03f;
+        [Range(0.0f, 1.0f)]
+        public float DensityCoef = 0.15f;
+        [Range(0.0f, 1.0f)]
+        public float ExtinctionCoef = 0.18f;
+        [Range(1, 64)]
+        public int SampleCount = 6;
+        [Range(0.0f, 0.999f)]
+        public float MieG = 0.32f;
+    }
+    public Settings setting = new Settings();
+
+
+    public SampleDepthTexPass m_SampleDepthTexPass { get; private set; }
+    public RayMarchLightPass m_RayMarchLightPass { get; private set; }
+    public ApplyPass m_BlitAddPass { get; private set; }
+
+
+    public override void Create()
+    {
+        //renderPass = new VolumetricLightsRenderPass();
+
+        m_SampleDepthTexPass = new SampleDepthTexPass();
+        m_SampleDepthTexPass.m_VolumtericResolution = setting.resolution;
+
+        m_RayMarchLightPass = new RayMarchLightPass();
+        m_RayMarchLightPass.m_VolumtericResolution = setting.resolution;
+        m_RayMarchLightPass.m_Feature = this;
+
+        m_BlitAddPass = new ApplyPass();
+        m_BlitAddPass.m_VolumtericResolution = setting.resolution;
+        m_BlitAddPass.m_Feature = this;
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(m_SampleDepthTexPass);
+
+        m_RayMarchLightPass.Setup(setting);
+        renderer.EnqueuePass(m_RayMarchLightPass);
+
+        m_BlitAddPass.Setup(renderer);
+        renderer.EnqueuePass(m_BlitAddPass);
+    }
+}
